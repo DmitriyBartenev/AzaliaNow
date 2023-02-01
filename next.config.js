239b419c724ea16fb5763df0a35faa0e7b873af3
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-02-01 20:06:47 +0400

    modified next config js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
   images:{
     domains: ['fakestoreapi.com']
-    
   }
 }
 
